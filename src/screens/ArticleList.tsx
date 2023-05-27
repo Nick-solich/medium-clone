d058f5f1f2803f95fd20b6eec1bf327dec85bce2
Nick <nick.rattanaton@gmail.com>
2023-05-27 15:19:38 +0700

    🎒 ArticlesList Global

--- src/screens/ArticleList.tsx
+++ src/screens/ArticleList.tsx
@@ -1,4 +1,23 @@
+import ArticleListSidebar from "components/ArticleListSidebar";
+import ArticlePreview from "components/ArticlePreview";
+import { DefaultResponseInterface } from "utils/api/default";
+import { articlesRequest, ArticlesInterface } from "utils/api/articles";
+import { useEffect, useState } from "react";
+
 export default function ArticleList(): JSX.Element {
+  const [articles, setArticles] = useState<ArticlesInterface>();
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    const response: DefaultResponseInterface = await articlesRequest();
+    if (response.ok) {
+      setArticles(JSON.parse(response.message));
+    }
+  };
+
   return (
     <>
       <div className="home-page">
@@ -26,83 +45,13 @@ export default function ArticleList(): JSX.Element {
                   </li>
                 </ul>
               </div>
-
-              <div className="article-preview">
-                <div className="article-meta">
-                  <a href="/#/profile/ericsimmons">
-                    <img src="http://i.imgur.com/Qr71crq.jpg" />
-                  </a>
-                  <div className="info">
-                    <a href="/#/profile/ericsimmons" className="author">
-                      Eric Simons
-                    </a>
-                    <span className="date">January 20th</span>
-                  </div>
-                  <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                    <i className="ion-heart" /> 29
-                  </button>
-                </div>
-                <a href="/#/how-to-build-webapps-that-scale" className="preview-link">
-                  <h1>How to build webapps that scale</h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                </a>
-              </div>
-
-              <div className="article-preview">
-                <div className="article-meta">
-                  <a href="/#/profile/albertpai">
-                    <img src="http://i.imgur.com/N4VcUeJ.jpg" />
-                  </a>
-                  <div className="info">
-                    <a href="/#/profile/albertpai" className="author">
-                      Albert Pai
-                    </a>
-                    <span className="date">January 20th</span>
-                  </div>
-                  <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                    <i className="ion-heart" /> 32
-                  </button>
-                </div>
-                <a href="/#/the-song-you-wont-ever-stop-singing" className="preview-link">
-                  <h1>The song you won&lsquo;t ever stop singing. No matter how hard you try.</h1>
-                  <p>This is the description for the post.</p>
-                  <span>Read more...</span>
-                </a>
-              </div>
+              {articles?.articles.map((article, idx) => (
+                <ArticlePreview key={idx} article={article} />
+              ))}
             </div>
 
             <div className="col-md-3">
-              <div className="sidebar">
-                <p>Popular Tags</p>
-
-                <div className="tag-list">
-                  <a href="" className="tag-pill tag-default">
-                    programming
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    javascript
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    emberjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    angularjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    react
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    mean
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    node
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    rails
-                  </a>
-                </div>
-              </div>
+              <ArticleListSidebar />
             </div>
           </div>
         </div>
