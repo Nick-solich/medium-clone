b7cde51d57f0ba2326bc34a4c0d14f463f68f901
Nick <nick.rattanaton@gmail.com>
2023-05-27 15:21:58 +0700

    🌹 Prettier Code

--- src/components/ArticleListSidebar.tsx
+++ src/components/ArticleListSidebar.tsx
@@ -17,15 +17,13 @@ export default function ArticleListSidebar(): JSX.Element {
   return (
     <div className="sidebar">
       <p>Popular Tags</p>
-      {tags && (
-        <div className="tag-list">
-          {tags.map(tag => (
-            <a key={tag} href="" className="tag-pill tag-default">
-              {tag}
-            </a>
-          ))}
-        </div>
-      )}
+      <div className="tag-list">
+        {tags?.map(tag => (
+          <a key={tag} href="" className="tag-pill tag-default">
+            {tag}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
